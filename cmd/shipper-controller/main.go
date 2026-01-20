c1fc1ac8d809b763143313c5d385ec6be02b105f
Amine <aminebakkali94@gmail.com>
2026-01-20 16:20:13 +0100

    starting empty manager

--- cmd/shipper-controller/main.go
+++ cmd/shipper-controller/main.go
@@ -4,6 +4,8 @@ package main
 import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"k8s.io/apimachinery/pkg/runtime"
+	"os"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 var (
@@ -13,12 +15,20 @@ var (
 
 func main() {
 
+	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+
 	mgr, err := ctrl.NewManager( ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme: scheme,	
 	})
 
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
 	}
 
 }
