62adbba9f715727c21f8f0626998856001f2abc4
Amine <aminebakkali94@gmail.com>
2026-01-21 14:56:21 +0100

    init environment controller

--- internal/controllers/shipper-controller/environment_controller.go
+++ internal/controllers/shipper-controller/environment_controller.go
@@ -6,6 +6,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 
@@ -16,6 +17,10 @@ type EvironmentReconciler struct {
 }
 
 func (r *EvironmentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error){
+
+	logger := log.FromContext(ctx)
+
+	var environment corev1alpha1.Encironment
 
 	return ctrl.Result{}, nil
 
