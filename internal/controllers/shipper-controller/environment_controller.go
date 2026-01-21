08a76641de68fae1aa41a200f98814ff663ddc0a
Amine <aminebakkali94@gmail.com>
2026-01-21 12:34:01 +0100

    init environment controller

--- internal/controllers/shipper-controller/environment_controller.go
+++ internal/controllers/shipper-controller/environment_controller.go
@@ -2,8 +2,10 @@ package shippercontroller
 
 
 import (
+	"context"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 
@@ -11,4 +13,10 @@ import (
 type EvironmentReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+}
+
+func (r *EvironmentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error){
+
+	return ctrl.Result{}, nil
+
 }
